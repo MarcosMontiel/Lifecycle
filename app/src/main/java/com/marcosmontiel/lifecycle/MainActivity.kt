391f5c583b5e70786bf688cd0fa4c0926d72f9d6
Marcos Montiel <marcos-gab14@hotmail.com>
2023-07-26 18:01:24 -0600

    Soundtrack file was added and implemented. UI was modified

--- app/src/main/java/com/marcosmontiel/lifecycle/MainActivity.kt
+++ app/src/main/java/com/marcosmontiel/lifecycle/MainActivity.kt
@@ -1,18 +1,27 @@
 package com.marcosmontiel.lifecycle
 
+import android.media.MediaPlayer
 import android.os.Bundle
+import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Surface
-import androidx.compose.material.Text
+import androidx.compose.foundation.layout.*
+import androidx.compose.material.*
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.window.Dialog
 import com.marcosmontiel.lifecycle.ui.theme.LifecycleTheme
 
 class MainActivity : ComponentActivity() {
+
+    private var _position: Int = 0
+    private lateinit var _mediaPlayer: MediaPlayer
 
     override fun onCreate(savedInstanceState: Bundle?) {
 
@@ -25,16 +34,96 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colors.background
                 ) {
-                    Greeting("Android")
+                    DialogCard()
+                    Log.i("Lifecycle", "onCreate")
                 }
             }
         }
 
     }
 
+    override fun onStart() {
+        super.onStart()
+        _mediaPlayer = MediaPlayer.create(this, R.raw.soundtrack)
+        Log.i("Lifecycle", "onStart")
+    }
+
+    override fun onResume() {
+        super.onResume()
+        _mediaPlayer.seekTo(_position)
+        _mediaPlayer.start()
+        Log.i("Lifecycle", "onResume")
+    }
+
+    override fun onPause() {
+        super.onPause()
+        _mediaPlayer.pause()
+        _position = _mediaPlayer.currentPosition
+        Log.i("Lifecycle", "onPause")
+    }
+
+    override fun onStop() {
+        super.onStop()
+        _mediaPlayer.stop()
+        _mediaPlayer.release()
+        Log.i("Lifecycle", "onStop")
+    }
+
+    override fun onRestart() {
+        super.onRestart()
+        Log.i("Lifecycle", "onRestart")
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        Log.i("Lifecycle", "onDestroy")
+    }
+
 }
 
 @Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
+fun DialogCard() {
+
+    var showDialog by rememberSaveable { mutableStateOf(false) }
+
+    Box(
+        modifier = Modifier.fillMaxSize(),
+        contentAlignment = Alignment.Center,
+        content = {
+
+            Card(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(8.dp),
+                content = {
+
+                    Column(
+                        modifier = Modifier.fillMaxWidth(),
+                        verticalArrangement = Arrangement.Center,
+                        horizontalAlignment = Alignment.CenterHorizontally
+                    ) {
+
+                        Text(text = "Lifecycle")
+
+                        Button(onClick = { showDialog = true }) {
+                            Text(text = "Show details")
+                        }
+
+                        if (showDialog) {
+
+                            Dialog(onDismissRequest = { showDialog = false }) {
+                                Box(modifier = Modifier.fillMaxWidth()) {
+                                    Text(text = "Hello")
+                                }
+                            }
+
+                        }
+
+                    }
+
+                }
+            )
+
+        }
+    )
 }
